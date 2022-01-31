e9d6eb7f0d43d2b325ced7de5f6b994be58e2652
vasil <vaskonikolov2003@gmail.com>
2022-01-31 17:59:11 +0200

    fucjibg covid

--- 1_intro/section_2/chapter_5/3_vector_data/vector_data.py
+++ 1_intro/section_2/chapter_5/3_vector_data/vector_data.py
@@ -85,13 +85,39 @@ def challenge_3():
     countries.plot(ax=ax1, color='white',  edgecolor='red')
     coastlines.plot(ax=ax1, color='darkblue')
     
-    cities.plot(column='pop_min',
+    cities.plot(column='pop_max',
                 legend=True,
                 ax=ax1,
-                cax=cax)
+                cax=cax, scheme='quantiles')
     
     plt.show()
 
+def bonus_challenge():
+    coastlines_path = os.path.join("data", "earthpy-downloads",
+                                "ne_50m_coastline",
+                                "ne_50m_coastline.shp")
 
+    coastlines = gpd.read_file(coastlines_path)
+    # print(coastlines.head())
+
+    populated_places_path = os.path.join("data", "earthpy-downloads",
+                                        "ne_50m_populated_places_simple",
+                                        "ne_50m_populated_places_simple.shp")
+
+    cities = gpd.read_file(populated_places_path)
+
+    countries_path = os.path.join("data", "earthpy-downloads",
+                              "ne_50m_admin_0_countries", 
+                              "ne_50m_admin_0_countries.shp")
+    
+    countries = gpd.read_file(countries_path)
+
+    f, ax1 = plt.subplots(figsize=(10, 6))
+
+    french_boundary = countries.loc[countries["SOVEREIGNT"] == "United States of America"]
+    french_boundary.plot(ax=ax1)
+    plt.show()
+    
+    plt.show()
 if __name__ == "__main__":
-    challenge_3()
+    bonus_challenge()
